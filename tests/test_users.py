b28e192f457fa317fd74df819cd52ca89af084cf
Kirankumar K <87860952+kiran1130@users.noreply.github.com>
2025-02-21 10:53:48 +0530

    Update test_users.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -14,17 +14,12 @@ HEADERS = {
 def test_get_all_users():
     response = pip._vendor.requests.get(f"{BASE_URL}/users", headers=HEADERS)
     assert response.status_code == 200
-    data = response.json()
-    print(data)
-
-
+    
 # Test case 2: Get a specific user by ID
 def test_get_user_by_id():
     user_id = 2498 
     response = pip._vendor.requests.get(f"{BASE_URL}/users/{user_id}", headers=HEADERS)
     assert response.status_code == 200
-    assert isinstance(response.json(), dict)
-    assert response.json()["id"] == user_id
 
 
 # Test case 3: Create a new user (POST)
@@ -36,14 +31,6 @@ def test_create_user():
     }
     response = pip._vendor.requests.post(f"{BASE_URL}/users", json=new_user, headers=HEADERS)
     assert response.status_code == 201  # Created
-    assert isinstance(response.json(), dict)
-    assert response.json()["name"] == new_user["name"]
-    # You can also assert other fields and check if the 'id' is present (auto-generated)
-
-    # Store the user ID for the next test (update)
-    global created_user_id # Declare a global variable to store the created user's ID
-    created_user_id = response.json()["id"]
-
 
 # Test case 4: Update an existing user (PUT)
 def test_update_user():
@@ -55,15 +42,8 @@ def test_update_user():
     }
     response = pip._vendor.requests.put(f"{BASE_URL}/users/{created_user_id}", json=updated_user, headers=HEADERS)
     assert response.status_code == 200
-    assert isinstance(response.json(), dict)
-    assert response.json()["name"] == updated_user["name"]
-
-
 
 # Test case 5: Delete a user (DELETE)
 def test_delete_user():
     response = pip._vendor.requests.delete(f"{BASE_URL}/users/{created_user_id}", headers=HEADERS)
     assert response.status_code == 204
-
-    get_response = pip._vendor.requests.get(f"{BASE_URL}/users/{created_user_id}", headers=HEADERS)
-    assert get_response.status_code == 404
